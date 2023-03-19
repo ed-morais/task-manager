1c375f22562f00131953b0903e216556c304a7e4
Edilson Morais <moraisbritoedilson@gmail.com>
2023-03-19 20:17:00 -0300

    feat: add post request

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
+const tasks = require('./routes/tasks')
 
-const port = 3000
+// middleware
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send(`Task manager app`)
 })
 
+app.post('/', (req, res) => {
+  res.send('mary Jane')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+const port = 3000
 app.listen(port, console.log(`Server is listening on port ${port}...`))
