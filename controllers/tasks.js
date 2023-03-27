d670445c28aabddeb1d62e311ffcba7f5f612ed1
Edilson Morais <moraisbritoedilson@gmail.com>
2023-03-27 10:45:46 -0300

    feat: add controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,16 @@
+const Task = require('../models/Task')
+
 const getAllTasks = (req, res) => {
   res.send('all items from the file')
 }
 
-const createTask = (req, res) => {
-  res.send('create task')
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body)
+  res.status(201).json({ task })
 }
 
 const getTask = (req, res) => {
-  res.send('get task')
+  res.json({ id: req.params.id })
 }
 
 const updateTask = (req, res) => {
